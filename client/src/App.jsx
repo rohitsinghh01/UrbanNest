50cb4229eb076cf2aa32859df756317eea46f325
Rohit Raj <singh.rohitraj122@gmail.com>
2024-07-02 15:00:43 +0530

    search bar added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,47 @@
-import './layout.scss'
-import Navbar from './components/Navbar/Navbar';
 import HomePage from './routes/homePage/homePage';
-function App() {
-  return(
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ListPage from './routes/listPage/listPage';
+import Layout from './routes/layout/layput';
+import SinglePage from './routes/singlePage/singlePage';
+import ProfilePage from './routes/profilePage/profilePage';
+import Login from './routes/login/login';
+import Register from './routes/register/register';
 
-   
-  <div className='layout'>
-    <Navbar />
-    <HomePage/>
-    
-    </div>
-  )
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          path: '/',
+          element: <HomePage />,
+        },
+        {
+          path: '/list',
+          element: <ListPage />,
+        },
+        {
+          path: '/:id',
+          element: <SinglePage />,
+        },
+        {
+          path: '/profile',
+          element: <ProfilePage />,
+        },
+        {
+          path: '/login',
+          element: <Login />,
+        },
+        {
+          path: '/register',
+          element: <Register />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
