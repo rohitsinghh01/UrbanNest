363a5c253ee476ee860baf42be5f21769ad8d45e
Rohit Raj <singh.rohitraj122@gmail.com>
2024-07-03 10:50:28 +0530

    listpage added

--- client/src/routes/listPage/listPage.jsx
+++ client/src/routes/listPage/listPage.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { listData } from '../../components/lib/dummyData';
+import './listPage.scss';
+import Filter from '../../components/filter/Filter';
+import Card from '../../components/card/Card';
+// import Map from '../../components/map/Map';
 
-export default function listPage() {
+function ListPage() {
+  const data = listData;
+
   return (
-    <div>
-      ListPage
+    <div className='listPage'>
+      <div className='listContainer'>
+        <div className='wrapper'>
+          <Filter />
+          {data.map((item) => (
+            <Card key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+      <div className='mapContainer'>
+        {/* <Map items={data} /> */}
+      </div>
     </div>
-  )
+  );
 }
+
+export default ListPage;
