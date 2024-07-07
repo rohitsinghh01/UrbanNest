14494827d17ed04517994323539ffe8b26a32554
Rohit Raj <singh.rohitraj122@gmail.com>
2024-07-07 12:45:49 +0530

    chat message logic implemented

--- server/app.js
+++ server/app.js
@@ -4,7 +4,8 @@ import cookieParser from 'cookie-parser';
 import authRoute from './routes/authRoute.js';
 import postRoute from './routes/postRoute.js';
 import userRoute from './routes/userRoute.js';
-
+import chatRoute from './routes/chatRoute.js';
+import messageRoute from './routes/messageRoute.js';
 
 const app = express();
 
@@ -15,6 +16,8 @@ app.use(cookieParser());
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/posts', postRoute);
+app.use('/api/chats', chatRoute);
+app.use('/api/messages', messageRoute);
 
 app.listen(3000, () => {
   console.log('Server is running!');
